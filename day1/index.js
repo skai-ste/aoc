dd44ab8e5e12b6b59afa28491e4b3383f7fb737b
skai-ste <skaiste.sibone@gorillasapp.com>
2022-01-20 18:09:37 +0000

    Refactor

--- day1/index.js
+++ day1/index.js
@@ -5,12 +5,13 @@ const part1 = (fileName) => {
   const file = fs
     .readFileSync(__dirname + "/" + fileName)
     .toString("utf8")
-    .split("\n");
+    .split("\n")
+    .map((item) => parseInt(item));
 
   let count = 0;
   for (let i = 1; i < file.length; i++) {
-    let measurement = parseInt(file[i]);
-    let prevMeasurement = parseInt(file[i - 1]);
+    let measurement = file[i];
+    let prevMeasurement = file[i - 1];
     if (measurement > prevMeasurement) {
       count += 1;
     }
@@ -22,14 +23,13 @@ const part2 = (fileName) => {
   const file = fs
     .readFileSync(__dirname + "/" + fileName)
     .toString("utf8")
-    .split("\n");
+    .split("\n")
+    .map((item) => parseInt(item)); // convert string in array to be numbers
 
   let count = 0;
   for (let i = 0; i < file.length - 3; i++) {
-    let measurement =
-      parseInt(file[i + 1]) + parseInt(file[i + 2]) + parseInt(file[i + 3]);
-    let prevMeasurement =
-      parseInt(file[i]) + parseInt(file[i + 1]) + parseInt(file[i + 2]);
+    let measurement = file[i + 1] + file[i + 2] + file[i + 3];
+    let prevMeasurement = file[i] + file[i + 1] + file[i + 2];
     if (measurement > prevMeasurement) {
       count += 1;
     }
