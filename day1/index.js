33471ef8603154d1528070dbe51700d0097050a2
skai-ste <skaiste.sibone@gorillasapp.com>
2022-01-20 17:58:52 +0000

    Second part of Day1

--- day1/index.js
+++ day1/index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-const solve = (fileName) => {
+const part1 = (fileName) => {
   // __dirname - get the current directory of the current file.
   const file = fs
     .readFileSync(__dirname + "/" + fileName)
@@ -18,4 +18,23 @@ const solve = (fileName) => {
   return count;
 };
 
-module.exports = { solve };
+const part2 = (fileName) => {
+  const file = fs
+    .readFileSync(__dirname + "/" + fileName)
+    .toString("utf8")
+    .split("\n");
+
+  let count = 0;
+  for (let i = 0; i < file.length - 3; i++) {
+    let measurement =
+      parseInt(file[i + 1]) + parseInt(file[i + 2]) + parseInt(file[i + 3]);
+    let prevMeasurement =
+      parseInt(file[i]) + parseInt(file[i + 1]) + parseInt(file[i + 2]);
+    if (measurement > prevMeasurement) {
+      count += 1;
+    }
+  }
+  return count;
+};
+
+module.exports = { part1, part2 };
--- day1/test.js
+++ day1/test.js
@@ -1,11 +1,19 @@
 const test = require("ava");
 
-const { solve } = require("./"); // require something in current directory
+const { part1, part2 } = require("./"); // require something in current directory
 
-test("checks example", (t) => {
-  t.is(solve("test-input.txt"), 7);
+test("checks example part1", (t) => {
+  t.is(part1("test-input.txt"), 7);
 });
 
-test("checks full data", (t) => {
-  t.is(solve("input.txt"), 1581);
+test("checks full data part1", (t) => {
+  t.is(part1("input.txt"), 1581);
+});
+
+test("checks example part2", (t) => {
+  t.is(part2("test-input.txt"), 5);
+});
+
+test("checks full data part2", (t) => {
+  t.is(part2("input.txt"), 1618);
 });
