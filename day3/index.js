9d9ab36ce65da1b361450b584559255ee117212a
skai-ste <skaiste.sibone@gorillasapp.com>
2022-03-26 16:30:48 +0100

    Day 3 part 2

--- day3/index.js
+++ day3/index.js
@@ -17,4 +17,44 @@ const part1 = (fileName) => {
   return parseInt(state.gamaRate, 2) * parseInt(state.epsilonRate, 2);
 };
 
-module.exports = { part1 };
+const mostCommonValue = (column) => {
+  const numberOfOnes = Array.from(column.matchAll("1")).length; //returns how many 1ns
+  return numberOfOnes >= column.length / 2 ? "1" : "0";
+};
+
+const leastCommonValue = (column) => {
+  return mostCommonValue(column) === "0" ? "1" : "0";
+};
+
+const part2 = (fileName) => {
+  const file = readInput(__dirname, fileName);
+
+  let oxygenGeneratorRating = Array.from(file);
+  let co2ScrubberRating = Array.from(file);
+
+  for (let columnIndex = 0; columnIndex < file[0].length; columnIndex++) {
+    const oxygenColumn = oxygenGeneratorRating
+      .map((row) => row[columnIndex])
+      .join("");
+    const co2Column = co2ScrubberRating.map((row) => row[columnIndex]).join("");
+
+    const mostCommon = mostCommonValue(oxygenColumn);
+    const leastCommon = leastCommonValue(co2Column);
+
+    if (oxygenGeneratorRating.length > 1) {
+      oxygenGeneratorRating = oxygenGeneratorRating.filter(
+        (number) => number[columnIndex] === mostCommon
+      );
+    }
+
+    if (co2ScrubberRating.length > 1) {
+      co2ScrubberRating = co2ScrubberRating.filter(
+        (number) => number[columnIndex] === leastCommon
+      );
+    }
+  }
+  return (
+    parseInt(oxygenGeneratorRating[0], 2) * parseInt(co2ScrubberRating[0], 2)
+  );
+};
+module.exports = { part1, part2 };
--- day3/test.js
+++ day3/test.js
@@ -1,6 +1,6 @@
 const test = require("ava");
 
-const { part1 } = require("."); // require something in current directory
+const { part1, part2 } = require("."); // require something in current directory
 
 test("checks example part1", (t) => {
   t.is(part1("test-input.txt"), 198);
@@ -10,10 +10,10 @@ test("checks full data part1", (t) => {
   t.is(part1("input.txt"), 1092896);
 });
 
-// test("checks example part2", (t) => {
-//   t.is(part2("test-input.txt"), 230);
-// });
+test("checks example part2", (t) => {
+  t.is(part2("test-input.txt"), 230);
+});
 
-// test("checks full data part2", (t) => {
-//   t.is(part2("input.txt"), 2120734350);
-// });
+test("checks full data part2", (t) => {
+  t.is(part2("input.txt"), 4672151);
+});
